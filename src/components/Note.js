fd83ccb36f21444a8c30e11aa6bc8f81ceb9fe5d
deizee <deizee@mail.ru>
2021-07-23 14:08:56 +0500

    done

---
+++ src/components/Note.js
@@ -0,0 +1,25 @@
+import {useRef} from "react";
+import PropTypes from "prop-types";
+
+const Note = (props) => {
+    const ref = useRef();
+    const onDeleteBtnClickHandler = () => {
+        const {id} = ref.current.dataset;
+        props.deleteNote(id);
+    };
+
+    return (
+        <div ref={ref} className="note" data-id={props.id}>
+            <p className="note-text">{props.text}</p>
+            <div className="delete-btn" onClick={onDeleteBtnClickHandler}>x</div>
+        </div>
+    );
+};
+
+Note.propType = {
+    id: PropTypes.number.isRequired,
+    text:PropTypes.string.isRequired,
+    deleteNote: PropTypes.func.isRequired
+}
+
+export default Note;
